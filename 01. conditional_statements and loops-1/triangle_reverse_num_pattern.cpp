836d6c46f12b92ef256e484fc0d4bdc0a8dc207e
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-02-18 15:17:48 +0530

    commit intro

---
+++ 01. conditional_statements and loops-1/triangle_reverse_num_pattern.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=i;
+        while(j>=1)
+        {
+            cout<<j;
+            j--;
+        }
+        cout<<"\n";
+        i++;
+    }
+    return 0;}
+
+
+
