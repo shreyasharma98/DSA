179a95b6627fa0d589a06c8d4fc9e9c9a9b7b564
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-02 23:45:20 +0530

    Update bubble_sort.cpp

--- 05. Sorting/bubble_sort.cpp
+++ 05. Sorting/bubble_sort.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+
 void BubbleSort(int arr[], int n){
 	// Write your code here
 
     for(int i=0;i<n-1;i++)
     {
-        for(int j =0;j<n-1;j++)
+        for(int j =0;j<n-1-i;j++)
         {
             if(arr[j]>arr[j+1])
             {
@@ -17,18 +19,21 @@ void BubbleSort(int arr[], int n){
     }
 }
 
-int main() {
 
-	int size,val;
+
+int main(){
+
+	int size;
 	cin>>size;
-	int *input=new int[1+size];
+
+	int * input=new int[1+size];
 
 	for(int i=0;i<size;i++)
 		cin>>input[i];
+
 	BubbleSort(input,size);
+
 	for(int i=0;i<size;i++)
 		cout<<input[i]<<" ";
-	return 0;
+
 }
-
-
