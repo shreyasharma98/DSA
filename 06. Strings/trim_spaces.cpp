cc690671ead4a277b9fa1454043a515a092da302
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-04 23:27:39 +0530

    Update trim_spaces.cpp

--- 06. Strings/trim_spaces.cpp
+++ 06. Strings/trim_spaces.cpp
@@ -1,12 +1,25 @@
+#include<iostream>
+using namespace std;
+
 
 void trimSpaces(char input[]) {
   int j = 0;
-    for(int i=0;input[i]!='\0';i++)
-        {
-            if(input[i]!=' ')
-            {
-                input[j++] = input[i];
-            }
-        }
-    input[j] = '\0';
+  for(int i = 0 ;input[i] !='\0' ;i++)
+  {
+      if(input[i] != ' ')
+      {
+          input[j++] = input[i];
+      }
+  }
+  input[j] = '\0';
+
 }
+
+int main() {
+    char input[1000000];
+    cin.getline(input, 1000000);
+    trimSpaces(input);
+    cout << input << endl;
+}
+
+
