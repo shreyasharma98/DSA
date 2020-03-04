672861192089b2b22e28434c111420efd7672667
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-04 23:20:15 +0530

    Update check_pallindrome.cpp

--- 06. Strings/check_pallindrome.cpp
+++ 06. Strings/check_pallindrome.cpp
@@ -1,15 +1,28 @@
+#include<iostream>
 #include <cstring>
+using namespace std;
 bool checkPalindrome(char str[]){
-    
-    int start = 0,end = strlen(str)-1;
-    while(start<=end)
+    int start = 0 , end = strlen(str) -1;
+    while(start <= end)
     {
-        if(str[start] != str[end])
-        {
-            return false;
-        }
-        start++;
-        end--;
+     if(str[start] != str[end])
+     {
+         return false;
+     }
+     start++;
+     end--;
     }
-    return true;
 }
+
+int main(){
+
+  char str[10000];
+  cin.getline(str,10000);
+
+  if(checkPalindrome(str)){
+  	cout << "true" << endl;
+  }else{
+  cout << "false" << endl;
+  }
+}
+
