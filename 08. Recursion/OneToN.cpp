dc95aa701820315a937dfac893f540d5e708c9a3
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-17 00:09:05 +0530

    Create OneToN.cpp

---
+++ 08. Recursion/OneToN.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void print(int n)
+{
+if(n == 0){return;}
+else{
+    print(n-1);
+    cout<<n<<" ";
+}
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
