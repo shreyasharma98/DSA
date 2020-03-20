492cccd3ba98e34ef77283a707cb6e6b7e744800
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-20 18:19:14 +0530

    Create coutn_zeros.cpp

---
+++ 08. Recursion/coutn_zeros.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+int countZeros(int n) {
+    if(n == 0)
+    {
+        return 0;
+    }
+    int ans  = countZeros(n/10);
+    if(n %10 == 0)
+    {
+        ans+=1;
+    }
+    return ans;
+
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countZeros(n) << endl;
+}
