e5c35325e774ad574c578641e8808390ff200a45
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-03-27 02:22:38 +0530

    Create tower_of_hanoi.cpp

---
+++ 09. Recursion-2/tower_of_hanoi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+   if(n == 1)
+   {
+       cout<<source<<" "<<destination<<"\n";
+       return;
+   }
+   if(n>1)
+   {
+       towerOfHanoi(n-1,source,destination,auxiliary);
+       cout<<source<<" "<<destination<<"\n";
+       towerOfHanoi(n-1,auxiliary,source,destination);
+   }
+
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    towerOfHanoi(n, 'a', 'b', 'c');
+}
