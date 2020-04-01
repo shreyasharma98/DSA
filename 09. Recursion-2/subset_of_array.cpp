505cb4f062f31c13b5141bf5742ca36cd4c9f1cc
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-04-01 18:15:27 +0530

    Update subset_of_array.cpp

--- 09. Recursion-2/subset_of_array.cpp
+++ 09. Recursion-2/subset_of_array.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
-int subset_helper(int input[],int n,int output[][20],int i)
-{
+
+int subset(int input[], int n, int output[][20]) {
     if(n == 0)
     {
-        output[i][0] = 0;
+        output[0][0] = 0;
         return 1;
     }
-    int ans = subset_helper(input+1,n-1,output,i++);
-    output[i][0] = ans;
-    for(int j=1;j<=ans;j++)
+    int ans = subset(input+1,n-1,output);
+    for(int i = 0;i<ans;i++)
     {
-        output[i][j] = input[0];
+        output[i+ans][0] = output[i][0]+1;
+        output[i+ans][1] = input[0];
     }
-    return ans*2;
-}
-int subset(int input[], int n, int output[][20]) {
-    return subset_helper(input,n,output,0);
+    for(int i = 0;i<ans;i++)
+    {
+        for(int j = 2;j<=output[i+ans][0];j++)
+        {
+            output[i+ans][j] = output[i][j-1];
+        }
+    }
+    return 2*ans;
 }
 
 int main() {
@@ -26,6 +30,7 @@ int main() {
     cin >> input[i];
 
   int size = subset(input, length, output);
+
 
   for( int i = 0; i < size; i++) {
 	for( int j = 1; j <= output[i][0]; j++) {
