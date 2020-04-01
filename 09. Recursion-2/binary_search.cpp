d1d5e9468703ddfbd2b0bfa65cb2c1f9170ca695
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-04-01 18:16:12 +0530

    Update binary_search.cpp

--- 09. Recursion-2/binary_search.cpp
+++ 09. Recursion-2/binary_search.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
 
+int binarySearchhelper(int input[],int start,int end,int element)
+{
+    if(start<=end)
+    {
+        int mid = (start+end)/2;
+        if(input[mid] == element)
+        {
+            return mid;
+        }
+        if(input[mid] > element)
+        {
+            return binarySearchhelper(input,start,mid-1,element);
+        }
+
+        return binarySearchhelper(input,mid+1,end,element);
+    }
+    return -1;
+}
 int binarySearch(int input[], int size, int element) {
-    int mid = (0+size)/2;
-    if(element<input[0]||element>input[size-1])
+    if(size == 0)
     {
         return -1;
     }
-    if(input[mid] == element)
-    {
-        return mid;
-    }
-
-    else if(input[mid] < element)
-        {
-            return (mid+1)+binarySearch(input+mid+1,size-mid-1,element);
-        }
-    else
-        {
-            return binarySearch(input,mid+1,element);
-        }
-    }
+ return binarySearchhelper(input,0,size-1,element);
+}
 
 int main() {
-    int input[100000],length,element, ans;
+    int input[500],length,element, ans;
     cin >> length;
     for(int i =0;i<length;i++)
     {
