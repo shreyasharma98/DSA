1a688bc66b66a9cc6e777795542b6b9855626e0b
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-04-08 01:40:29 +0530

    Create print_permutations.cpp

---
+++ 09. Recursion-2/print_permutations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void printPermutations_helper(string input,string output)
+{
+    if(input.length()==0)
+    {
+        cout<<output<<endl;
+        return ;
+    }
+
+    for(int i = 0;i<input.length();i++)
+    {
+        printPermutations_helper(input.substr(0,i-1)+input.substr(i+1),output+input[i]);
+    }
+}
+void printPermutations(string input){
+
+    	printPermutations_helper(input,"");
+}
+
+int main(){
+    string input;
+    cin >> input;
+    printPermutations(input);
+    return 0;
+}
