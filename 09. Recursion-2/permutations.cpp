957e7ec066f1433597a192947839fdb1be932664
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-04-09 22:08:27 +0530

    Update permutations.cpp

--- 09. Recursion-2/permutations.cpp
+++ 09. Recursion-2/permutations.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int returnPermutations(string input, string output[]){
-    if(input)
+    if(input.length() == 1)
+    {
+        output[0] = input[0];
+        return 1;
+    }
+    int k = 0;
+    for(int i = 0;i<input.length();i++)
+    {
+        char ch = input[i];
+        string ros = input.substr(0,i)+input.substr(i+1);
+        string new_output[1000];
+        int x = returnPermutations(ros,new_output);
+        for(int j=0;j<x;j++)
+        {
+            output[k++] = ch+new_output[j];
+        }
+        if(i == input.length()-1)
+        {
+            return input.length()*x;
+        }
+    }
 }
 
 int main(){
