52d761161e6637cdc38c12df933af99af9ab449a
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-05-29 02:34:14 +0530

    Update k_reverse.cpp

--- 12.Linked_List/k_reverse.cpp
+++ 12.Linked_List/k_reverse.cpp
@@ -48,3 +48,40 @@ node* kReverse(node*head,int n)
     ans.tail->next = h3;
     return ans.head;
 }
+node* takeinput(){
+    int data;
+    cin>>data;
+    node* head=NULL,*tail=NULL;
+    while(data!=-1){
+        node *newnode=new node(data);
+        if(head==NULL)                  {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            tail=newnode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    node* head=takeinput();
+    int n;
+    cin>>n;
+    head=kReverse(head,n);
+    print(head);
+    return 0;
+}
