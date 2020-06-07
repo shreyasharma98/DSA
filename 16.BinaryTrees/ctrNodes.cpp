47dfb4870fc3308fd30c1732e0f31a269f6313f8
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-06-08 00:43:17 +0530

    Create ctrNodes.cpp

---
+++ 16.BinaryTrees/ctrNodes.cpp
@@ -0,0 +1,55 @@
+#include<queue>
+#include<iostream>
+#include "BinaryTree.h"
+using namespace std;
+int countNodes(BinaryTreeNode<int>* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    return 1+ countNodes(root->left)+ countNodes(root->right);
+}
+BinaryTreeNode<int>* TakeInput()
+{
+    queue<BinaryTreeNode<int>*> pendingQ ;
+    int rootData;
+    cout<<"Enter Root data : "<<endl;
+    cin>>rootData;
+    if(rootData == -1)
+        return NULL;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    pendingQ.push(root);
+    while(!pendingQ.empty())
+    {
+        BinaryTreeNode<int>* top = pendingQ.front();
+        pendingQ.pop();
+        int leftData,rightData;
+        cout<<"Enter left of "<<top->data<<" : "<<endl;
+        cin>>leftData;
+        if(leftData != -1)
+        {
+            BinaryTreeNode<int>* lt = new BinaryTreeNode<int>(leftData);
+            top->left = lt;
+            pendingQ.push(lt);
+        }
+        cout<<"Enter right of "<<top->data<<" : "<<endl;
+        cin>>rightData;
+        if(rightData != -1)
+        {
+            BinaryTreeNode<int>* rt = new BinaryTreeNode<int>(rightData);
+            top->right = rt;
+            pendingQ.push(rt);
+        }
+
+    }
+    return root;
+}
+int main()
+{
+    BinaryTreeNode<int>* root = TakeInput();
+    cout<<countNodes(root);
+    delete root;
+    return 0;
+}
+
