b929803d57678293af8b8656d75c78deae836fd6
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-06-15 02:16:31 +0530

    Create classBSTuse.cpp

---
+++ 17.BinarySearchTrees/classBSTuse.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include "classBST.h"
+using namespace std;
+
+int main(){
+    BST *tree = new BST();
+    int choice, input;
+    while(true){
+        cin>>choice;
+        switch(choice){
+            case 1:
+                cin >> input;
+                tree->insert(input);
+                break;
+            case 2:
+                cin >> input;
+                tree->deleteNode(input);
+                break;
+            case 3:
+                cin >> input;
+                if(tree->hasData(input)) {
+                    cout << "true" << endl;
+                }
+                else {
+                    cout << "false" << endl;
+                }
+                break;
+            default:
+                tree->print();
+                return 0;
+                break;
+        }
+    }
+}
