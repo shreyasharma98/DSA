74de008fd12d96a43788cb8e9cc9a60a37caa717
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-06-22 00:21:38 +0530

    Create kLargest.cpp

---
+++ 18.Priority Queues/kLargest.cpp
@@ -0,0 +1,48 @@
+#include<queue>
+#include<vector>
+#include<iostream>
+using namespace std;
+
+vector<int> kLargest(int input[], int n, int k){
+priority_queue<int , vector<int> , greater<int> > pq;
+    for(int i = 0;i<k;i++)
+    {
+        pq.push(input[i]);
+    }
+    for(int  i=k;i<n;i++)
+    {
+        if(input[i] > pq.top())
+        {
+            pq.pop();
+            pq.push(input[i]);
+        }
+    }
+    vector<int> V;
+    while(!pq.empty())
+    {
+        V.push_back(pq.top());
+        pq.pop();
+    }
+    return V;
+
+}
+
+
+int main() {
+
+	int size;
+	cin >> size;
+	int *input = new int[1 + size];
+
+	for(int i = 0; i < size; i++)
+		cin >> input[i];
+
+    int k;
+    cin >> k;
+
+    vector<int> output = kLargest(input, size, k);
+    for(int i = 0; i < output.size(); i++)
+        cout << output[i] << endl;
+
+	return 0;
+}
